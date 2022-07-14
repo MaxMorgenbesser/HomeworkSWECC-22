ffc252479c5ee616fcbfdbf006d0d7ff2d8825d7
Max Morgenbesser <maxwellmorgenbesser@gmail.com>
2022-07-14 10:40:17 -0400

    created homeork folder

---
+++ bocacode/homework/day-three.js
@@ -0,0 +1,16 @@
+function addNum(num1,num2){
+    return num1+num2
+}
+
+let car = {
+    model:"Toyota Corolla",
+    color:"silver",
+}
+car.year=99
+ console.log(car.model)
+ console.log(car.color)
+
+ function firstArr(arr){
+  return arr[0]
+ }
+ console.log(firstArr([1,2,3,4]))
